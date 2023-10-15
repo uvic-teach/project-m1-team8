5b99dc73c502d7d8139a05051ff61774f1095af8
Miles <106453865+milesrose13@users.noreply.github.com>
2023-10-15 02:27:38 -0700

    Update ERBooking.py

--- UIMicroservice/components/ERBooking.py
+++ UIMicroservice/components/ERBooking.py
@@ -1,13 +1,28 @@
-import datetime
-
 class ERBooking:
-    def __init__(self, user_id: int, medical_id: int, booking_time: string, booking_location: Hospital):
+    def __init__(self, user_id: int, medical_id: int, booking_id: int, booking_time: timedelta, booking_location: string):
       self.user_id = user_id
       self.medical_id = medical_id   
+      self.booking_id = booking_id
       self.booking_time = booking_time
-      self.booking_location = booking_location
-      booking_info = str(booking_time) + "at" + str(booking_location)
+      self.booking_location = booking_location #String of Hospital name/address
+      booking_info = str(booking_time) + "at" + booking_location
       NotificationService.create_notification(user_id, "Your ER Booking time is" + booking_info)
 
+    def cancel_booking(self, user_id, booking_id):
+        #implementation to cancel a users ERBooking instance 
+
+    def get_user_id(self):
+        return self.user_id
+
+    def get_medical_id(self):
+        return self.medical_id
+
+    def get_booking_id(self):
+        return self.booking_id
+
+    def get_booking_location(self)
+        return self.booking_location
+
+    
     
       
