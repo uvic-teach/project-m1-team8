2569460b0d5245ce868a7e4f64a9d08e7b0a1eee
Miles <106453865+milesrose13@users.noreply.github.com>
2023-10-15 02:01:13 -0700

    Create ERBooking.py

---
+++ UIMicroservice/components/ERBooking.py
@@ -0,0 +1,9 @@
+import datetime
+
+class ERBooking:
+    def __init__(self, user_id: int, medical_id: int, booking_time: string):
+      self.user_id = user_id
+      self.medical_id = medical_id   
+      self.booking_time = booking_time
+      
+      
