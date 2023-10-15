82e84b42491056961e6a1acbb6027a7b6338f805
Miles <106453865+milesrose13@users.noreply.github.com>
2023-10-15 14:32:46 -0700

    Update main.py

--- UIMicroservice/main.py
+++ UIMicroservice/main.py
@@ -66,6 +66,22 @@ async def cancel_er_booking(booking_id: int):
         "user_id": user_id
   }
 
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="Custom title",
+        version="2.5.0",
+        summary="This is a very custom OpenAPI schema",
+        description="Here's a longer description of the custom **OpenAPI** schema",
+        routes=app.routes,
+    )
+    openapi_schema["info"]["x-logo"] = {
+        "url": "https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png"
+    }
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
 
 
 app.openapi = custom_openapi
