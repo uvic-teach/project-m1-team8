4156e51fce8352ba04ffeac9161b6ca0f4eb69d7
Miles <106453865+milesrose13@users.noreply.github.com>
2023-10-15 01:55:17 -0700

    Create notification.py

---
+++ UIMicroservice/components/notification.py
@@ -0,0 +1,13 @@
+class Notification():
+  
+  def __init__(self, user_id: int, message: string):
+    self.user_id = user_id
+    self.message = message
+
+  def get_message(self):
+      return self.message
+
+  def get_user_id(self):
+    return self.user_id
+    
+    
