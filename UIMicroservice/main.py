3b935dc74258a619bd19462208ab955d0a0ac829
Miles <106453865+milesrose13@users.noreply.github.com>
2023-10-15 13:15:58 -0700

    Update main.py

--- UIMicroservice/main.py
+++ UIMicroservice/main.py
@@ -11,13 +11,26 @@ app = FastAPI()
   async def homepage():
     return "MisterED User Interface"
 
-@app.get("/notificationSvc/{user_id}")
+@app.get("/notification/{user_id}")
 async def get_notifications(user_id: int)
     return {
             "user_id": user_id
             "notification_list": []
            }
-@app.post("/notificationSvc/{user_id}")
+
+@app.post("/notification/{user_id}")
 async def add_notification(user_id: int, message: string):
-    # notificationService.create_notification(user_id, message)
+    # NotificationService.create_notification(user_id, message) append to notification_list 
+  return {"message": Notification was successfully added"}
+
+@app.post("/er/loadTime/{hospital_id}")
+async def check_er_load(hospital_id: int):
+  return {"message": "Current wait time at Chilliwack General Hospital is 2 hours"}
+
+@app.post("/er/booking/request")
+async def create_er_request(patient_id: int, priority_level: int):
+  return {"Current wait time at Chilliwack General Hospital is 2 hours"}
+
+
+
 
