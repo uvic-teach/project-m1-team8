08ff0d6f2300103339ff21bb150f5c63c13f9867
Miles <106453865+milesrose13@users.noreply.github.com>
2023-10-15 01:27:41 -0700

    Update NotificationService.py

--- UIMicroservice/Services/NotificationService.py
+++ UIMicroservice/Services/NotificationService.py
@@ -1,1 +1,9 @@
+class NotificationService:
+  def __init__(self):
+    NotificationList = [];
 
+  def get_notifications(self, IncomingNotifications) -> NotificationList
+        #scan through incoming notifications from internal services to see if new notifications have been requested.
+    pass
+
+  
