8fbbd3bef4afc2e32565a94aa276e1be70911e72
Oliver <122414170+OllieWare@users.noreply.github.com>
2023-10-15 00:31:56 -0700

    first draft Authentication.py

--- UIMicroservice/Services/Authentication.py
+++ UIMicroservice/Services/Authentication.py
@@ -1,1 +1,37 @@
+class Authentication():
+  def _init_(self):
+    pass
+    
+  def request_validation(self,username:str,password: str)-> void:
+    """
+    Request a user validation from AccountInfoManagement then updates the page based on validity
+    Args:
+    username(str): Username of the user
+    password(str): Password of the user
+    changes to page:
+    On Valid: enter user dashboard page
+    On Invalid: reload login page with error notification
+    Returns:
+    none
+    """
+    pass
+  def request_account_creation(self, account_info: AccountInformation, health_info: UserHealthInfo) -> void:
+     """
+    Request account creation from AccountInfoManagement, then load the login page 
+    and send notification of account creation success failure
+    Args:
+    username(str): Username of the user
+    password(str): Password of the user
+
+    Changes to page:
+    On success:
+    load login page and display success notification
+    On failure:
+    load register page and display error notification
+    Returns:
+    none
+    """
+    
+    
+    pass
 
