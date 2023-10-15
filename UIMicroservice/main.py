a4f9335cd4453293b7552c4c6f67a8eddbfaff18
Miles <106453865+milesrose13@users.noreply.github.com>
2023-10-15 13:02:51 -0700

    Update main.py

--- UIMicroservice/main.py
+++ UIMicroservice/main.py
@@ -1,12 +1,23 @@
 from fastapi import FastAPI
 from Services import ERService, Authentication, NotificationService, TriageDataGetter
+from components import notification
 
 app = FastAPI()
+#notificationService = NotificationService()
 
 #functions definition
 
 @app.get("/")
   async def homepage():
-    return "MisterED UI"
+    return "MisterED User Interface"
 
+@app.get("/notificationSvc/{user_id}")
+async def get_notifications(user_id: int)
+    return {
+            "user_id": user_id
+            "notification_list": []
+           }
+@app.post("/notificationSvc/{user_id}")
+async def add_notification(user_id: int, message: string):
+    # notificationService.create_notification(user_id, message)
 
