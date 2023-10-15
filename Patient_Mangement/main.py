47880fcf280a790d39f7d91d8b69301ec79734a8
minhn201 <99488911+minhn201@users.noreply.github.com>
2023-10-15 13:46:17 -0700

    Update main.py

--- Patient_Mangement/main.py
+++ Patient_Mangement/main.py
@@ -1,6 +1,7 @@
 import bcrypt
 from fastapi import FastAPI, HTTPException, status
-from pydantic import BaseModel
+from fastapi.openapi.utils import get_openapi
+from pydantic import BaseModel, Field
 import random
 import string
 # from services import AccountInfoManagement, TriageManagement
@@ -8,6 +9,32 @@ import string
 app = FastAPI()
 # manageTriage = TriageManagement()
 # manageAccount = AccountInfoManagement()
+
+class LoginResponse(BaseModel):
+    success: bool = Field(example=True)
+    message: str = Field(example="Login successful")
+
+class AccountCreationResponse(BaseModel):
+    message: str = Field(example="Account created successfully.")
+    patient_id: str = Field(example="12345")
+    user_name: str = Field(example="John Doe")
+    username: str = Field(example="johndoe12345")
+
+class UserHealthInfoResponse(BaseModel):
+    patient_id: str = Field(example="12345")
+    height: float = Field(example=175.3)
+    weight: float = Field(example=68.5)
+    blood_type: str = Field(example="A+")
+    blood_pressure: float = Field(example=120.8)
+    allergies: str = Field(example="Peanuts, Shellfish")
+    complications: str = Field(example="Hypertension, Diabetes Type 2")
+
+class TriageDetailResponse(BaseModel):
+    patient_id: str = Field(example="12345")
+    triage_id: str = Field(example="45382")
+    available_date: str = Field(example="1944-06-06")
+    summary: str = Field(example="44-year-old male with acute chest pain, ...")
+
 
 class LoginData(BaseModel):
     username: str
@@ -38,6 +65,38 @@ class AccountCreationRequest(BaseModel):
     account_info: AccountInformationCreation
     health_info: UserHealthInfoCreation
 
+class TriageHistoryItem(BaseModel):
+    patient_id: str = Field(example="12345")
+    triage_id: str = Field(example="45382")
+    available_date: str = Field(example="1944-06-06")
+    summary: str = Field(example=("44-year-old male with acute chest pain, shortness of breath, and sweating presented. Vital signs: BP 160/95 mmHg, HR 110 bpm, RR 22 breaths/min, Temp 98.6°F, O2 Sat 92%. Pain described as sharp, radiating to left arm, rated 8/10. ECG showed ST-segment elevation in leads II, III, aVF, categorized as Triage Level 2. Administered chewable aspirin, nitroglycerin, prepared for further emergency procedures. Family updated."))
+
+class TriageListResponse(BaseModel):
+    triage_history: list[TriageHistoryItem] = Field(
+        example={"triage_history":[
+            {
+                "patient_id": "12345",
+                "triage_id": "45382",
+                "available_date": "1944-06-06",
+                "summary": "44-year-old male with acute chest pain, shortness of breath, and sweating presented. Vital signs: BP 160/95 mmHg, HR 110 bpm, RR 22 breaths/min, Temp 98.6°F, O2 Sat 92%. Pain described as sharp, radiating to left arm, rated 8/10. ECG showed ST-segment elevation in leads II, III, aVF, categorized as Triage Level 2. Administered chewable aspirin, nitroglycerin, prepared for further emergency procedures. Family updated."
+            
+            },
+            {
+                "patient_id": "12345",
+                 "triage_id": "10439",
+                "available_date": "1975-07-15",
+                "summary": "75-year-old male, Triage Level 1, showed severe respiratory distress, cyanosis, altered consciousness. Immediate airway management, high-flow oxygen initiated, critical care team mobilized for advanced support."
+            },
+            {
+                "patient_id": "12345",
+                "triage_id": "86241",
+                "available_date": "1980-03-23",
+                "summary": "80-year-old male, Triage Level 2, with persistent chest pain, mild dyspnea, history of hypertension. Administered nitroglycerin, continuous ECG monitoring, cardiology alerted for comprehensive cardiac assessment."
+            }
+        ]
+        }
+    )
+
 
 def validate_user(username: str, password: str) -> dict:
     hashed_password = bcrypt.hashpw("js1544".encode('utf-8'), bcrypt.gensalt())
@@ -47,7 +106,7 @@ def validate_user(username: str, password: str) -> dict:
         return {"success": False, "message": "Invalid username or password"}
 
 
-@app.post("/login/")
+@app.post("/login/", response_model=LoginResponse)
 async def user_validation(data: LoginData):
     #manageAccount.validate_user(username, password)
     result = validate_user(data.username, data.password)
@@ -62,7 +121,7 @@ def generate_username(name: str) -> str:
     username = name.lower().replace(" ", "") + random_string
     return username
 
-@app.post("/create-account/")
+@app.post("/create-account/", response_model=AccountCreationResponse)
 async def create_new_account(request: AccountCreationRequest):
     #manageAccount.create_account(patient_id)
     account_info = request.account_info
@@ -82,7 +141,7 @@ async def create_new_account(request: AccountCreationRequest):
     return message
 
 
-@app.get("/{patient_id}/")
+@app.get("/{patient_id}/", response_model=UserHealthInfoResponse)
 async def get_user_health_info(patient_id):
     #manageAccount.get_user_info(patient_id)
     return {
@@ -96,7 +155,7 @@ async def get_user_health_info(patient_id):
     }
     
 
-@app.get("/{patient_id}/triages/")
+@app.get("/{patient_id}/triages/", response_model=TriageListResponse)
 async def get_triage_list(patient_id):
     #mangeTriage.getTriageRecordList(patient_id)
     return {"triage_history":
@@ -120,7 +179,7 @@ async def get_triage_list(patient_id):
 }] }
 
 
-@app.get("/{patient_id}/triages/{triage_id}")
+@app.get("/{patient_id}/triages/{triage_id}", response_model=TriageDetailResponse)
 async def get_specific_triage(patient_id, triage_id):
      #mangeTriage.getTriageRecord(patient_id, triage_id)
     return {
@@ -129,3 +188,22 @@ async def get_specific_triage(patient_id, triage_id):
     "available_date": "1944-06-06",
     "summary": "44-year-old male with acute chest pain, shortness of breath, and sweating presented. Vital signs: BP 160/95 mmHg, HR 110 bpm, RR 22 breaths/min, Temp 98.6°F, O2 Sat 92%. Pain described as sharp, radiating to left arm, rated 8/10. ECG showed ST-segment elevation in leads II, III, aVF, categorized as Triage Level 2. Administered chewable aspirin, nitroglycerin, prepared for further emergency procedures. Family updated."
   }
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="Custom title",
+        version="2.5.0",
+        summary="This is a very custom OpenAPI schema",
+        description="Here's a longer description of the custom **OpenAPI** schema",
+        routes=app.routes,
+    )
+    openapi_schema["info"]["x-logo"] = {
+        "url": "https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png"
+    }
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
