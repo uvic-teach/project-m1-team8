00c017ed882f35485920b50252dc714d5f708971
Miles <106453865+milesrose13@users.noreply.github.com>
2023-10-15 13:48:01 -0700

    Update main.py

--- UIMicroservice/main.py
+++ UIMicroservice/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from Services import ERService, Authentication, NotificationService, TriageDataGetter
-from components import notification
+from components import notification, booking
 
 app = FastAPI()
 #notificationService = NotificationService()
@@ -19,17 +19,50 @@ async def get_notifications(user_id: int)
            }
 
 @app.post("/notification/{user_id}")
-async def add_notification(user_id: int, message: string):
+async def create_notification(user_id: int, message: string):
     # NotificationService.create_notification(user_id, message) append to notification_list 
-  return {"message": Notification was successfully added"}
+  return {"message": Notification was successfully created"}
 
 @app.post("/er/loadTime/{hospital_id}")
 async def check_er_load(hospital_id: int):
   return {"message": "Current wait time at Chilliwack General Hospital is 2 hours"}
 
+@app.get("er/booking/{booking_id}):
+async def get_er_booking(booking_id: int)
+  #get booking id from erService
+  return {
+      "patient_id": user_id,
+      "booking_id": booking_id,
+      "hospital_id": hospital_id,
+      "booking_time": booking_timedelta,
+      "hospital_name": "Chilliwack General Hospital",
+      "estimated_wait": "2 hours",
+      "queue_position": 6
+  }
+
 @app.post("/er/booking/request")
-async def create_er_request(patient_id: int, priority_level: int):
-  return {"Current wait time at Chilliwack General Hospital is 2 hours"}
+async def create_er_request(patient_id: int, ):
+  #erService does work to create booking
+  return {
+          "message": "Booking request successfull.",
+          "patient_id": user_id,
+          "booking_id": booking_id,
+          "hospital_id": hospital_id,
+          "booking_time": booking_timedelta,
+          "hospital_name": "Chilliwack General Hospital",
+          "estimated_wait": "2 hours",
+          "queue_position": 6
+        }
+
+@app.post("/er/booking/{booking_id}/cancelbooking")
+async def cancel_er_booking(booking_id: int):
+  #clear booking from erqueue
+  return {
+        "message": "Booking cancellation request successfull.",
+        "booking_id": booking_id
+        "user_id": user_id
+  }
+         
 
 
 
