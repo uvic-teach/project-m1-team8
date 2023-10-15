47dcd9a9f83cacc60cf75843af463861da903f2d
Miles <106453865+milesrose13@users.noreply.github.com>
2023-10-15 02:15:15 -0700

    Update ERBooking.py

--- UIMicroservice/components/ERBooking.py
+++ UIMicroservice/components/ERBooking.py
@@ -1,9 +1,13 @@
 import datetime
 
 class ERBooking:
-    def __init__(self, user_id: int, medical_id: int, booking_time: string):
+    def __init__(self, user_id: int, medical_id: int, booking_time: string, booking_location: Hospital):
       self.user_id = user_id
       self.medical_id = medical_id   
       self.booking_time = booking_time
-      
+      self.booking_location = booking_location
+      booking_info = str(booking_time) + "at" + str(booking_location)
+      NotificationService.create_notification(user_id, "Your ER Booking time is" + booking_info)
+
+    
       
