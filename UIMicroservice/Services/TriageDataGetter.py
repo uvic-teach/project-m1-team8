11f475136deb1f0b3b15920922408bd68810b720
Oliver <122414170+OllieWare@users.noreply.github.com>
2023-10-15 01:41:27 -0700

    First Draft TriageDataGetter.py

--- UIMicroservice/Services/TriageDataGetter.py
+++ UIMicroservice/Services/TriageDataGetter.py
@@ -1,1 +1,38 @@
+class TriageDataHandler():
+  def _init_(self):
+    #initialize anything necessary
+    pass
+  def GrabnDisplayList(self, patient_id:str)-> void:
+    """
+    request the patients triage instances list from TriageManagement and 
+    display it on the webpage
+    Args:
+      patient_id(str): ID of patient to check
+    Changes to page:
+      load triage history page a table containing the full 
+      list of triage instances
+    Returns:
+      none
+    """
+    pass
+   def GrabnDisplayElement(self, patient_id:str, triage_id:str )-> void:
+    """
+    request the patients triage instances list from TriageManagement and 
+    display it on the webpage
+    Args:
+      patient_id(str): ID of patient to check
+    Changes to page:
+      load triage history page with a table containing the one selected
+      element from the list of triage instances
+    Returns:
+      none
+    """
+    pass
+    
+
+
+  
+
+
+
 
