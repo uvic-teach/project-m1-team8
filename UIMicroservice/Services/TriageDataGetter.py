1ec21a0a079b28e2dbbc56c9c19332f3c6b13a55
Oliver <122414170+OllieWare@users.noreply.github.com>
2023-10-15 20:54:54 -0700

    added perform triage

--- UIMicroservice/Services/TriageDataGetter.py
+++ UIMicroservice/Services/TriageDataGetter.py
@@ -2,7 +2,7 @@ class TriageDataHandler():
   def _init_(self):
     #initialize anything necessary
     pass
-  def GrabnDisplayList(self, patient_id:str)-> void:
+  def GrabnDisplayList(self, patient_id:str)-> None:
     """
     request the patients triage instances list from TriageManagement and 
     display it on the webpage
@@ -15,7 +15,7 @@ class TriageDataHandler():
       none
     """
     pass
-   def GrabnDisplayElement(self, patient_id:str, triage_id:str )-> void:
+   def GrabnDisplayElement(self, patient_id:str, triage_id:str )-> None:
     """
     request the patients triage instances list from TriageManagement and 
     display it on the webpage
@@ -28,6 +28,18 @@ class TriageDataHandler():
       none
     """
     pass
+  def performTriage(self, patient_id:str)->None:
+    """
+    request for a triage to occur using TriageManagement
+    args:
+    patient_id(str): the id of the patient
+    Returns:
+    none
+    """
+    pass
+    
+
+
     
 
 
