ca74517c17919ec24918c6b6fb566d0c37f3eebf
Oliver <122414170+OllieWare@users.noreply.github.com>
2023-11-04 20:41:36 +0000

    create rollback stubs

---
+++ UIMicroservice/components/Rollback.py
@@ -0,0 +1,11 @@
+class Rollback():
+    pageName= ""
+    def __init__(self):
+        #initialise first system values
+        pass
+    def updateState(self):
+        #for updating the state after accomplishing a task
+        pass
+    def Restore(self):
+        #restore to previous save state
+        pass
