2e34ca19e77aca6df175a7cbdb4e8824f9f103ec
Miles <106453865+milesrose13@users.noreply.github.com>
2023-11-11 18:15:08 -0800

    major changes to FastAPI backend

--- UIMicroserviceFastAPI/database.py
+++ UIMicroserviceFastAPI/database.py
@@ -1,3 +1,4 @@
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
---
+++ UIMicroserviceFastAPI/schemas.py
@@ -0,0 +1,35 @@
+from pydantic import BaseModel
+import datetime
+
+
+
+class UserCreate(BaseModel):
+    username: str
+    email: str
+    password: str
+
+class booking(BaseModel):
+    booking_id: str
+    username: str
+    hospital_name: str
+
+
+class requestdetails(BaseModel):
+    email:str
+    password:str
+        
+class TokenSchema(BaseModel):
+    access_token: str
+    refresh_token: str
+
+class changepassword(BaseModel):
+    email:str
+    old_password:str
+    new_password:str
+
+class TokenCreate(BaseModel):
+    user_id:str
+    access_token:str
+    refresh_token:str
+    status:bool
+    created_date:datetime.datetime
